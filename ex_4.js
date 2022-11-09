83a5d22fadc81d5d049eb370cc6f099ed277e6ea
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-11-09 14:00:50 +0400

    Comment

--- ex_4.js
+++ ex_4.js
@@ -2,6 +2,9 @@ const arr = [1, 2, 3];
 
 let result = [];
 
+// for (let index = arr.length - 1; index >= 0; index--) {
+   // result += (arr[index])}
+   
 for (let index = arr.length - 1; index >= 0; index--) {
    result.push(arr[index])
 }
