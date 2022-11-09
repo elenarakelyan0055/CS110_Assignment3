2a83a071c09174823c8f80bea5db0691e79049c8
MacBook <macbook@MacBooks-MacBook-Pro.local>
2022-11-09 12:43:21 +0400

    first commit

---
+++ ex_1.js
@@ -0,0 +1,17 @@
+let totalLines = 3;
+
+let curLine = 1;
+
+let text = '';
+
+while (curLine <= totalLines) {
+    text+="+";
+    curLine++;
+}
+
+let curRow = 1;
+while (curRow <= totalLines) {
+    console.log(text);
+     curRow++;
+}
+
---
+++ ex_2.js
@@ -0,0 +1,2 @@
+let grade = 80 
+
